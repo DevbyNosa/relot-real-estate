6d55fbe8f05738b627681b3faeaa29818be75603
Judges Nosakhare Igbinosa <judgesnigbinosa@gmail.com>
2026-03-27 21:26:31 +0100

    Update config

--- config/settings.js
+++ config/settings.js
@@ -2,12 +2,7 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 const db = new Pool({
-  user:  process.env.DB_USER,
-  host:  process.env.DB_HOST,
-  database:  process.env.DATABASE,
-  password:   process.env.DB_PASSWORD ,
-  port: 5432 || process.env.DB_PORT  ,
-
+  connectionString: process.env.DATABASE_URL,
   
  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 
